48960879acf45025a7fa98dc66ac8724fc7c89ff
m0slah <muhammedslahqadir@gmail.com>
2025-01-25 13:49:53 +0300

    add Container component to fetch and display posts

---
+++ src/components/Container.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default function Container() {
+  const [data, setData] = useState<IPost[] | null>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<unknown>(null);
+
+  const getPorst = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      setData(data);
+
+      console.log("data");
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getPorst();
+  }, []);
+
+  return isLoading ? (
+    <span>Loading... </span>
+  ) : data ? (
+    <ul>
+      {data.map((post: IPost) => (
+        <li key={`item-${post.id}`}>
+          <span>{post.title}</span>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <span>{JSON.stringify(error)}</span>
+  );
+}
